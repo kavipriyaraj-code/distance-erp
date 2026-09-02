c61de1325deb114754baabfef16f6f9c53263d1d
Kavipriya <kavipriyaraj2004@gmail.com>
2026-09-02 06:51:51 +0530

    Add Vercel deployment: api/index.py WSGI handler, vercel.json, .env.example

--- distance_erp/settings.py
+++ distance_erp/settings.py
@@ -103,11 +103,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STORAGES = {
-    'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
-    },
-}
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
